9aa6ee12f370e4e5503680b9c575ec5039c9d9a7
sothunsreynoch <sothunsreynoch@gmail.com>
2023-05-04 22:45:34 +0700

    homework_table

--- pages/compo/demo.js
+++
@@ -1,5 +0,0 @@
-// export default function demo(){
-//  return(
-//     <h1>Hello World</h1>
-//  )
-// }
--- pages/index.js
+++ pages/index.js
@@ -3,58 +3,115 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Link from 'next/link'
+import DataTable , { createTheme } from 'react-data-table-component'
 import Layout from './compo/layout'
-import { API_KEY } from './lib/constant'
-import { BASE_PATH } from './lib/constant'
-import { BASE_URL } from './lib/constant'
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import React , { useEffect,useState } from 'react'
 
 
 
 const inter = Inter({ subsets: ['latin'] })
 
- export default function Home({movies}) {
+ export default function Home() {
+
+  const [product, setProduct] = useState([])
+  const [search , setSearch] = useState(" ");
+  const [allProducts ,setallProducts] = useState([]);
+  const getProducts = async () => {
+         const res = await axios.get(`https://api.escuelajs.co/api/v1/products/`)
+        setProduct(res.data)
+        setallProducts(res.data)
+        console.log(product)
+  }
+
+  const edit = () => {
+    toast.warn("Edit is success !" , {
+      theme : "colored",
+      autoClose: 1000,
+      position: "bottom-right",
+    });
+  };
+
+  const deleted = () => {
+    toast.error("Delete is success !" , {
+      theme : "colored",
+      autoClose: 1000,
+      position: "bottom-right",
+    });
+  };
+
+  const columns= [
+    {
+        name: "Product Name",
+        selector: row => row.title,
+        sortable: true,
+    },
+    {
+        name: "Price",
+        selector: row => row.price,
+    },
+    {
+      name: "Category",
+      selector: row => row.category.name,
+  },
+    {
+        name: "Photo",
+        selector: row => <img src={row.images} width={100} />
+        ,
+    },
+    {
+      name: "Action Edit",
+      cell: (row) =>  <button className='btn btn-warning' onClick={edit}>Edit</button>
+      ,
+    },
+    {
+      name: "Action Delete",
+      cell: (row) =>  <button className='btn btn-danger' onClick={deleted}>Delete</button>
+      ,
+    },
+]
+
+  useEffect(() => {
+      getProducts();
+  },[])
+
+  useEffect(() => {
+      const result = product.filter(products => {
+        return products.title.toLowerCase().match(search.toLowerCase());
+      });
+      setallProducts(result);
+  },[search])
+
+
   return (
-    <Layout>
-    <div className='container'>
-            <div className="row g-3">
-                {
-                  movies.results.map(movie => (
-                      <div className="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2">
-                        <Link href={{
-                          pathname: `/movies/${movie.id}`,
-                          query: {
-                            movie: JSON.stringify(movie)
-                          }
-                        }} 
-                        as={`/movies/${movie.id}`}
-                        className="text-decoration-none">
-                        <div className="card border-0 shadow-sm h-100">
-                            <img src={BASE_PATH + movie.backdrop_path} className="card-img-top" alt="..." />
-                            <div className="card-body">
-                                <p className="card-text">{movie.title}</p>
-                            </div>
-                        </div>
-                        </Link>
-                      </div> 
-                  ))
-                }
-            </div>
-        </div>
+    <Layout className='bg-white'>
+    <main className='container bg-light '>
+        <h1> Products Collection in Table</h1>
+        <DataTable
+        title='All Products List'
+        columns={columns}
+        data={allProducts}
+        pagination
+        fixedHeader
+        fixedHeaderScrollHeight='500px'
+        subHeader
+        subHeaderComponent={
+          <input type='text'
+           placeholder='Search Here'
+            className='form-control w-25'
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}/>
+        }
+        />
+        <ToastContainer/>
+        </main>
         </Layout>
    
 
     
   )
 }
-export async function getServerSideProps(){
-  console.log(`${BASE_URL}movie/popular?api_key=${API_KEY}&page=1`)
-  const res = await fetch(`${BASE_URL}movie/popular?api_key=${API_KEY}&page=1`)
-  const resp = await res.json();
-  console.log(resp)
-  return{
-      props:{
-        movies:resp
-      }
-    }
-}
+
 
